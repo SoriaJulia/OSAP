701aebdd00a83edc7b8c1869f4e68be5048ddcd5
SoriaJulia <soriajulias@gmail.com>
2022-04-19 16:30:08 -0300

    fix navbars items

--- components/Navbar/ProfileMenu.tsx
+++ components/Navbar/ProfileMenu.tsx
@@ -1,12 +1,14 @@
 import { UserCircle, User, SignOut } from 'phosphor-react';
 import React from 'react';
+import { useAuth } from '../../context/AuthContext';
 import NavbarMenu from './NavbarMenu';
 import NavbarMenuItem from './NavbarMenuItem';
 
 function ProfileMenu() {
+  const { user } = useAuth();
   return (
     <NavbarMenu
-      text="Ingresar"
+      text={user?.name || ''}
       icon={<UserCircle weight="duotone" size={32} />}
     >
       <NavbarMenuItem
