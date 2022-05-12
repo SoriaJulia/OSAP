b93305446b8815d28dbebb6a308261c8e8bcd238
SoriaJulia <soriajulias@gmail.com>
2022-05-12 15:27:16 -0300

    add centros atencion

--- components/PublicSectionsNav.tsx
+++ components/PublicSectionsNav.tsx
@@ -7,7 +7,7 @@ const PublicSectionsNav = () => {
       <SectionButton href="cartilla" label="Cartilla médica" icon={UserList} variant="" />
       <SectionButton label="Consulta a distancia" icon={ChatsCircle} variant="" href="/consultaDistancia" />
       <SectionButton href="/Emergencias" label="Atencion de Emergencias" icon={FirstAid} variant="" />
-      <SectionButton href="/centrosatencion" label="Centros de atención" icon={Buildings} variant="" />
+      <SectionButton href="/centrosAtencion" label="Centros de atención" icon={Buildings} variant="" />
       <SectionButton href="/asistenciaAlViajero" label="Asistencia al viajero" icon={Car} variant="" />
       <SectionButton href="/faqs" label="Preguntas frecuentes" icon={Question} variant="" />
     </nav>
