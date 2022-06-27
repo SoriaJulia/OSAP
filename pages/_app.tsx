e09a77b21025695c95a95e0706b8ef7da9549310
SoriaJulia <soriajulias@gmail.com>
2022-06-27 11:41:55 -0300

    fix for demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ import Backdrop from 'components/Base/Backdrop';
 import PageLoader from 'components/Base/PageLoader';
 import Layout from '../components/Layout/Layout';
 
-const noLayoutPages = ['/'];
+const noLayoutPages = ['/index-login'];
 
 function MyApp({ Component: PageComponent, pageProps: { session, ...pageProps } }: AppProps) {
   const router = useRouter();
